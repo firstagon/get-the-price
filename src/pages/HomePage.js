41412b2f8c0532af33673277a20400038ac063e8
firstagon <98339053+firstagon@users.noreply.github.com>
2023-08-01 21:21:45 +0500

    started styling animation for welcome cards

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,7 @@
 // import classes from "./HomePage.module.css";
 
 import ItemsChoice from "../ui/ItemChoiceUI/ItemsChoice";
-import WelcomeBlock from "../ui/MainElements/WelcomeBlock";
+import WelcomeBlock from '../ui/MainElements/welcomeBlock';
 import RequestChoice from "../ui/RequestChoice.js/RequestChoice";
 import MainInfo from "../ui/MainElements/MainInfo";
 import JoinBlock from '../ui/MainElements/JoinBlock';
@@ -9,7 +9,7 @@ import JoinBlock from '../ui/MainElements/JoinBlock';
 
 const isAuth = false;
 
-const HomePage = ({userState}) => {
+const HomePage = ({ userState }) => {
   return (
     <div className={'mainBody'}>
       {/* <WelcomeBlock /> */}
--- src/ui/mainElements/JoinBlock.js
+++ src/ui/mainElements/JoinBlock.js
@@ -3,14 +3,17 @@ const JoinBlock = () => {
     <div className="joinSection">
       <div className="joinBlock">
         <div className="joinCard">
+
           <p className="joinText">Got account?</p>
           <button className="butt"> Sign in </button>
+
         </div>
-        <div className="line" />
         <div className="joinCard">
+
           <p className="joinText">New One?</p>
           <button className="butt"> Sign up </button>
         </div>
+
       </div>
     </div>
   );
