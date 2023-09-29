f8398dc8deaf1d08b7f067494b073bc6be4b0117
firstagon <98339053+firstagon@users.noreply.github.com>
2023-09-29 10:41:00 +0500

    fixed serv issue when not sending page when refresh not root page

--- serv.js
+++ serv.js
@@ -9,8 +9,8 @@ const PORT = process.env.APP_FRONTEND_PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "/build")));
 
-app.get('*', (req, res) => {
-    res.send(path.join(__dirname, '/build/index.html'))
+app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/build/index.html'))
 })
 
 app.listen(PORT);
